49afb90459e83116ac6f5f27d7cf88875d5a58fc
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2023-11-13 09:29:51 +0100

    change

--- Pac-Man/Program.cs
+++ Pac-Man/Program.cs
@@ -1,2 +1,54 @@
-﻿Console.WriteLine("PAC-MAN");
-Console.ReadLine();
+﻿using System;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// Console.BackgroundColor = ConsoleColor.Black;
+// Console.ForegroundColor = ConsoleColor.White;
+
+// Start();
+// Room1();
+// GameOver();
+// Finished();
+
+// static void Start()
+// { 
+//     Console.WriteLine("PAC-MAN");
+//     Console.ReadLine();
+// }
+
+// // första rummet
+// static void Room1()
+// {
+
+// }
+
+// static void GameOver()
+// {
+
+// }
+
+// static void Finished()
+// {
+
+// }
+
+
+Console.CursorVisible = false;
+int x = 0;
+int y = 0;
+Console.SetCursorPosition(x, y);
+
+while (true)
+{
+    ConsoleKey key = Console.ReadKey().Key;
+
+    Console.SetCursorPosition(x, y);
+    Console.Write(" ");
+
+    if (key == ConsoleKey.RightArrow) x++;
+    if (key == ConsoleKey.LeftArrow) x--;
+    if (key == ConsoleKey.DownArrow) y++;
+    if (key == ConsoleKey.UpArrow) x--;
+
+    Console.SetCursorPosition(x, y);
+    Console.Write("💀");
+}
