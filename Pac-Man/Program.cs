321f1a5d22d0fd0b7b25f23e1c7fa1eeded690e8
Vilma Evborn <vilma.evborn@elev.ga.ntig.se>
2023-11-15 10:57:30 +0100

    new change

--- Pac-Man/Program.cs
+++ Pac-Man/Program.cs
@@ -1,13 +1,119 @@
-﻿using System;
+﻿using Raylib_cs;
+using System.CodeDom.Compiler;
+using System.Linq.Expressions;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Random generator = new Random();
+
+Raylib.InitWindow(800, 600, "Pac-Man");
+Raylib.SetTargetFPS(60);
+
+int [,] snow = new int[10, 10];
+
+snow[0, 0] = 8;
+snow[5, 0] = 8;
+snow[5, 1] = 8;
+snow[5, 2] = 8;
+
+int space = 2;
+
+
+for (int y = 0; y < snow.GetLength(1); y++)
+{
+    for (int x = 0; x < snow.GetLength(0); x++)
+    {
+        Console.SetCursorPosition(x * space, y * space);
+        
+        Console.Write(snow[x, y]);
+    }
+}
+
+float speed = 5;
+int coins = 0;
+string scene = "start";
+
+while (!Raylib.WindowShouldClose())
+{
+    if (scene == "start")
+    {
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+        {
+            scene = "room1";
+        }
+    }
+    else if (scene == "room1")
+    {
+
+    }
+    else if (scene == "gameOver")
+    {
+
+    }
+    else if (scene == "finished")
+    {
+
+    }
+
+    //--------------------------------------------------------------------------------
+    //              DRAWING
+    //--------------------------------------------------------------------------------
+    
+    Raylib.BeginDrawing();
+    if (scene == "start")
+    {
+        Raylib.ClearBackground(Color.BLACK);    
+
+    }
+    else if (scene == "room1")
+    {
+        Raylib.ClearBackground(Color.BLACK);
+    }
+    else if (scene == "gameOver")
+    {
+        Raylib.ClearBackground(Color.GOLD);
+    }
+    else if (scene == "finished")
+    {
+        Raylib.ClearBackground(Color.BLACK);
+    }
+    Raylib.EndDrawing();
+}
+
+// using System;
+// using System.CodeDom.Compiler;
+// using System.Linq.Expressions;
+// using System.Numerics;
+// using System.Security.Cryptography.X509Certificates;
+// Console.OutputEncoding = System.Text.Encoding.UTF8;
+
 
 // Console.BackgroundColor = ConsoleColor.Black;
 // Console.ForegroundColor = ConsoleColor.White;
 
-// Start();
-// Room1();
-// GameOver();
-// Finished();
+// //spelplan
+// int [,] board = new int[16, 12];
+
+// board[0, 0] = 8;
+// board[5, 0] = 8;
+// board[5, 1] = 8;
+// board[5, 2] = 8;
+
+// int space = 2;
+
+
+// for (int y = 0; y < board.GetLength(1); y++)
+// {
+//     for (int x = 0; x < board.GetLength(0); x++)
+//     {
+//         Console.SetCursorPosition(x * space, y * space);
+        
+//         Console.Write(board[x, y]);
+//     }
+// }
+
+// Console.ReadLine();
 
 // static void Start()
 // { 
@@ -32,23 +138,24 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 // }
 
 
-Console.CursorVisible = false;
-int x = 0;
-int y = 0;
-Console.SetCursorPosition(x, y);
+// Console.CursorVisible = false;
+// int x = 0;
+// int y = 0;
+// Console.SetCursorPosition(x, y);
 
-while (true)
-{
-    ConsoleKey key = Console.ReadKey().Key;
+// while (true)
+// {
+//     ConsoleKey key = Console.ReadKey().Key;
 
-    Console.SetCursorPosition(x, y);
-    Console.Write(" ");
+//     Console.SetCursorPosition(x, y);
+//     Console.Write(" ");
 
-    if (key == ConsoleKey.RightArrow) x++;
-    if (key == ConsoleKey.LeftArrow) x--;
-    if (key == ConsoleKey.DownArrow) y++;
-    if (key == ConsoleKey.UpArrow) x--;
+//     if (key == ConsoleKey.RightArrow) x++;
+//     if (key == ConsoleKey.LeftArrow) x--;
+//     if (key == ConsoleKey.DownArrow) y++;
+//     if (key == ConsoleKey.UpArrow) x--;
 
-    Console.SetCursorPosition(x, y);
-    Console.Write("💀");
-}
+//     Console.SetCursorPosition(x, y);
+//     Console.Write("💀");
+//     Console.ReadLine();
+// }
